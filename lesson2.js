dca5224f30613dcc34f5f6532a2dc65398f75187
walwizard <goneisgone@hotmail.com>
2023-02-18 07:19:24 -0300

    Lasson 2

---
+++ lesson2.js
@@ -0,0 +1,10 @@
+//First JS Code
+console.log("Hello JS");
+
+
+//So how I can run it ??
+//Just opne ur broswer em press F12
+//OR in the right click 
+//search for 'DevTools"
+//inside dev tools search for CONSOLE
+//in this place you can use js
