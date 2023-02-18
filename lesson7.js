932c53df70cead250fe18f61bda85c90bf6ae9aa
walwizard <goneisgone@hotmail.com>
2023-02-18 08:12:35 -0300

    Lasson 7

---
+++ lesson7.js
@@ -0,0 +1,29 @@
+/*
+    Okay, once you have the VScode, and Node 
+    let's start with ceate a folder on your computer
+    choose on place that you always use 
+    Ex:
+ - documents
+ - Desktop
+
+ and name as Javascript, 
+ 
+ once with the vs code openned have a lot ways
+ to open a file  you can :
+ on the 'welcome' screen have a lot options (new file, open file)
+
+ or search for the top bar on the option 'FILE'
+ and go to OPEN FOLDER, and search for your folder
+
+ now lets create a file with the .js 
+*/
+
+console.log("Hello World learning JS");
+
+/**
+  to run it 
+  search on the top bar for 'terminal' > new terminal
+  on the terminal use node + your file name ex:
+  
+  > node hallo.js
+ */
