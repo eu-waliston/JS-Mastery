bb6a6cf371b0823fc92d56ba762621d30acb1819
walwizard <goneisgone@hotmail.com>
2023-02-18 07:34:42 -0300

    Lasson 3

---
+++ lesson3.js
@@ -0,0 +1,32 @@
+/*
+    #Let's talk about jabascript 😄
+
+    1️⃣ Javascript
+    - Formelly known as LiveScript in 1995
+    - Most Popular language used in web browsers
+    - Javascript is not JAVA 
+
+    2️⃣ Interpreted Language
+    - No need to compile to machine code
+    - slower
+
+    3️⃣ Dynamically typed language
+    - no types 
+    - difrent of the other program languages, 
+    - in js you dont need to declare a type ex:
+    - JS >> let = 20, let = "nome"
+    - Java >> int age = 20; , string nome = "java"; 
+
+    4️⃣ EcmaScript
+    - Specification tells how JavaScript should be
+    - implmented by the browser so that a JavaScript
+    - Program runs excatly the same in all browsers, but
+    - it does not tell how JavaScript should run inside 
+    - these browsers
+    - it is up to the browser vender to decide 
+
+    5️⃣ JavaScript Engine
+    - Every browser provides a JavaScript Engine to run
+    - your code.
+    - V8 is the most popular and also used in NoeJS
+*/
