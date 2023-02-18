50c9de380c4b49b6fd1d78f5e0f7958f3b53d68b
walwizard <goneisgone@hotmail.com>
2023-02-18 08:27:28 -0300

    Lasson 8

---
+++ lesson8.js
@@ -0,0 +1,37 @@
+/*
+ okay as you can see we can run 
+ codes on terminal
+ but sometimnes it's bored right ?
+ don't worry, vs code privides 
+ a lot of extensions to us
+ on the left bar search for extensions 
+ icons and open
+ here you can serach for: 
+
+ extensios 
+ themes 
+
+ and more...
+ the first extensions the we will use,
+ it's called 'Code Runner'
+ with it we can run js without always
+ open the console 
+ it's cool right ? 
+
+
+*/
+
+console.log(6 * 9);
+console.log('I used Code Runner 🔥');
+
+/**
+  here you have a lot of options to run the code 
+
+  on the right of the top bar, have a PLAY icon to 'run code'
+
+  you can click on it 
+
+  or on your right mouse button, search for 'Run Code'
+
+  or ctrl + alt + n
+ */
