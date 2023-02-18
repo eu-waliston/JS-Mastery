655952303fb014c3d8079512ff4185c736a3b1b2
walwizard <goneisgone@hotmail.com>
2023-02-18 08:01:48 -0300

    Lasson 6

--- lesson6.js
+++ lesson6.js
@@ -1,6 +1,6 @@
 /*
  As i supose you read the links that a let here,
- javascrip run on V8 , so to run JS outside the broser he 
+ javascrip run on V8 , so to run JS outside the broser we 
  use Node.JS, you need to install it to 
 
  https://nodejs.org/en/
