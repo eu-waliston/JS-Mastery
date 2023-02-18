d1554af188bbcb2db247388ed8b4cab5fad669b8
walwizard <goneisgone@hotmail.com>
2023-02-18 07:56:28 -0300

    Lasson 5

---
+++ lesson5.js
@@ -0,0 +1,15 @@
+/*
+Okay now that you know JS and your plataform etc
+Let's start for real
+We can't use the broser tools forever right ? haha
+So int this course  i will foccus on VSCode
+VsCode its the most popular open source and free
+code editor 
+here's the link:
+
+>> https://code.visualstudio.com/
+
+Avaiable for : Windows, mac and Linux
+
+Download and install and we meet on the nexts lesson 😎
+*/
