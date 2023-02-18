20f47e0cfda23e061a87559171fa9dd43ea8be42
walwizard <goneisgone@hotmail.com>
2023-02-18 08:40:45 -0300

    Lasson 9

---
+++ lesson9.js
@@ -0,0 +1,10 @@
+/**
+ * In this section ill talk about comments
+ * we have a lot types
+ * comments are important to tell for the people
+ * about ur code
+ * this is onde type
+ * a lot of lines comment
+ */
+
+//This is the one line comments 
