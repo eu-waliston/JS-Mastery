fe337939ae4646e3cc2481ab0534680b98cebd77
walwizard <goneisgone@hotmail.com>
2023-02-18 09:21:39 -0300

    Lasson 10

---
+++ lesson10.js
@@ -0,0 +1,14 @@
+//Lets talk about some concept on javascript language
+
+//in javascript you cant use double quotes on your codes
+console.log("Double")
+
+//but you can use single quotes
+console.log('Single')
+
+//Difrent by other program languages, in JS u don't need
+//to use semicolon on your codes
+//it's optional
+console.log('wiht - semicolon')
+console.log('wihtout - semicolon');
+//the code will run normally
