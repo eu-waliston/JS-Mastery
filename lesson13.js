186bdba00eff3181f08ac30e32e76aee9cdfd481
walwizard <goneisgone@hotmail.com>
2023-02-25 09:44:13 -0300

    lesson 13 naming variables

---
+++ lesson13.js
@@ -0,0 +1,19 @@
+//Naming Variables 
+/*
+TO name your variables, you need to
+follow some rules and recomendations
+for example your variables can't:
+- contain spaces
+
+they must:
+- begin with a letter and underscore (_)
+or a dollar sign ($)
+- names are case sensitive
+it means THIS are difrerent of this is JS
+
+and last 
+- you can't use reserved keywords
+and what is this ? here the link:
+
+https://www.w3schools.com/js/js_reserved.asp
+*/
