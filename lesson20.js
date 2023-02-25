4a4edf4224e33f4186e0d6476a28fb983b5a6a94
walwizard <goneisgone@hotmail.com>
2023-02-25 11:11:50 -0300

    lesson 20 Built in functions

---
+++ lesson20.js
@@ -0,0 +1,16 @@
+//Built in functions
+//This kinda function are native in JS
+//and can help us with a several things 
+
+let person = {
+    name: "James"
+}
+
+console.log(Object.values(person));
+console.log(Object.keys(person));
+
+console.log("Jamila".toUpperCase());
+console.log("Jamila".toLocaleLowerCase());
+console.log("Jamila".search("m"));
+console.log("Jamila".indexOf("l"));
+
