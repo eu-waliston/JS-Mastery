d7efef604f2ac559132324c788712e5f012c6e97
walwizard <goneisgone@hotmail.com>
2023-02-25 10:19:36 -0300

    lesson 17 ARRAYS

---
+++ lesson17.js
@@ -0,0 +1,18 @@
+//Arrays
+//This data type, in JS is used to store multiplo values
+//to declare an array you use squarebrackets []
+// like:
+let names = [
+    "Alex",
+    "Jamila",
+    "Joe",
+    "Aisha"
+];
+
+//Have a lot of ways to access itens
+//with the position:
+console.log(names[0]);
+//the full array: 
+console.log(names);
+
+console.log(names.length);
