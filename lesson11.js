3f51c13047666ccbd67d580f188762d5b0053e60
walwizard <goneisgone@hotmail.com>
2023-02-25 09:23:43 -0300

    lesson 11

---
+++ lesson11.js
@@ -0,0 +1,34 @@
+//Varaibles 
+//its a simples placeholder to your store your data type
+// In JS a data type can be:
+/*
+ String
+ Number
+ Boolean 
+ Date
+ Object
+ Undefined
+
+ to declare a varaible you  have three 'ways'
+ but i don't recomend the first one
+
+ var 
+ let 
+ const 
+ Example:
+
+ var nome = ""
+ let nome = ""
+ const nome = ""
+*/
+
+let firstName = "Jamila"
+let age = 21
+let isFemale = true
+let balance = 100.32
+let dob = new Date()
+let person = {}
+let empty = undefined
+
+//To check just uses console.log
+
