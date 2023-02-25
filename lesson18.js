60fc1ced4d18722edae12910a406c84ff3f305b6
walwizard <goneisgone@hotmail.com>
2023-02-25 10:45:05 -0300

    lesson 18 Arithmetic Operators

---
+++ lesson18.js
@@ -0,0 +1,22 @@
+// Arithmetic Operators
+/**
+ * In JS we have a lot of operators thry can be:
+ * addition         +
+ * subtraction      - 
+ * division         / 
+ * multiplications * 
+ * remainder       %
+ */
+
+let addition  = 5 + 9
+let subtraction = 5 - 9
+let division = 5 / 9
+let multiplications = 5 * 9
+let remainder = 10 % 2
+
+console.log(addition);
+console.log(subtraction);
+console.log(division);
+console.log(multiplications);
+console.log(remainder);
+
