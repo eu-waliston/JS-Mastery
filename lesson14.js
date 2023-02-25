899eda91323acb6d4c4b7ab755de41eaec2c81a0
walwizard <goneisgone@hotmail.com>
2023-02-25 09:52:45 -0300

    lesson 14 STRINGS

---
+++ lesson14.js
@@ -0,0 +1,10 @@
+//Strings
+/* 
+string is a data type  that allow us to store 
+a sequence of caratcters
+for example
+*/
+let brand = "Amigoscode"
+console.log(typeof brand);
+console.log(brand.length);
+console.log(brand.substring(1,6));
