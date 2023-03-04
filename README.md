fc927cf1e7b526802e27981d3d91199f85b783ff
walwizard <goneisgone@hotmail.com>
2023-03-04 09:21:58 -0300

    Lesson 28 - Logical Operator

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![JS MASTERY](https://user-images.githubusercontent.com/82295321/219524955-e33ebb56-eefb-4794-bbe0-4bc148bcc3d2.png)
 
-## In this repository, i will post 101 javascript lessons to improve myself and anyone that like JS
+## In this repository, i will post  javascript lessons to improve myself and anyone that like JS + Extras
 
 ## Frequency 
 - Only Weekends
---
+++ lesson28.js
@@ -0,0 +1,20 @@
+//Logical Operator
+
+/*
+    Reference: 
+    - https://www.w3schools.com/js/js_comparisons.asp
+
+    JS have 4 Logical operators
+    &&
+    ||
+    !
+    ??
+
+    for now ill talk about || and &&
+    this kinda opoeraators allows us to connect on or more 
+    expressions to build powerfull conditions
+    here's an example:
+*/
+
+console.log(10 == 10 && "10" == "10");
+console.log(10 == 10 || "10" == "10");
