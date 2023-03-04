d6f531dfea3034ec4c4f424ad246ac98d9f62fdd
walwizard <goneisgone@hotmail.com>
2023-03-04 09:05:26 -0300

    Lesson 27 - Comparison Operators

---
+++ lesson27.js
@@ -0,0 +1,18 @@
+//Comparison Operators
+/*
+In JS we have ways to compara the expressions
+An for this the comparison opeartors come toplay
+and the are:
+> <= > >= == !=
+an we can use this to compare a lot of types:
+Strtings, number ,object etc
+
+and they ill always return a boolean
+*/
+
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
