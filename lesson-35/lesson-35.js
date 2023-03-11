ff9d59c76b024c2453d4392ed183b9c9f24d169b
walwizard <goneisgone@hotmail.com>
2023-03-11 09:52:02 -0300

    When to Use Const vs Let

---
+++ lesson-35/lesson-35.js
@@ -0,0 +1,8 @@
+//When to Use Const vs Let
+
+/*
+    reference: https://www.freecodecamp.org/news/understanding-let-const-and-var-keywords/#:~:text=Use%20const%20more%20often.,a%20variable%20without%20declaring%20it.
+
+
+    Use const more often. Use let when you need to reassign another value to a variable. Don't try to access a variable without declaring it.
+*/
