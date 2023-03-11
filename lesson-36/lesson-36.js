2056b6a32bc003d6db9c4ff3e391087a665edf70
walwizard <goneisgone@hotmail.com>
2023-03-11 10:00:52 -0300

    Type Coercion

---
+++ lesson-36/lesson-36.js
@@ -0,0 +1,10 @@
+//Type Coercion
+
+// reference -> https://developer.mozilla.org/en-US/docs/Glossary/Type_coercion
+
+/*
+It's mean that JS will change the value automatically for us, if  the JS think that something its a number it will be a number an so on
+*/
+
+console.log(0 == false);
+console.log(0 == true);
