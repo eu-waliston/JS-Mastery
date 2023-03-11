922b99d56a3369494a6e29036a45c9a0e90c6955
walwizard <goneisgone@hotmail.com>
2023-03-11 09:48:26 -0300

    Const Keyword

---
+++ lesson-34/lesson-34.js
@@ -0,0 +1,17 @@
+//Const Keyword
+/*
+
+    reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+
+    A const in javascript referes a 'constant' value
+    we can't change the value of a const, but we can
+    add values to a constant for example:
+*/
+
+const brand = "AmigosCode";
+console.log(brand);
+const brandoOBJ = {};
+
+brandoOBJ["brand"] = brand;
+
+console.log(brandoOBJ);
