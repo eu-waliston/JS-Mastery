559d65e2c1a0bc946dc6ccc0e4e5b9330e5de373
walwizard <goneisgone@hotmail.com>
2023-03-11 09:23:30 -0300

    Switch Statement

---
+++ lesson-31/lesson-31.js
@@ -0,0 +1,19 @@
+//Switch Statement
+/*
+Another way to check the matc betwen two or more expressions is using the switch statement, it's exactly like the if but with little differences
+
+here's an example:
+*/
+
+let gender = "M"
+
+switch (gender) {
+    case "F":
+        console.log("F");
+        break;
+    case "M":
+        console.log("M");
+
+    default:
+        break;
+}
