e56e603dac6e920ddff6615b890a18f80a2a1755
walwizard <goneisgone@hotmail.com>
2023-03-25 17:15:16 -0300

    JS Mastery - Working with Arrays

---
+++ lesson-47/lesson-47.js
@@ -0,0 +1,19 @@
+//Working with Arrays
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.push(50)
+
+numbers.forEach((number) => {
+    console.log(number);
+})
+
+numbers.splice(3, 1)
+
+numbers.forEach((number) => {
+    console.log(number);
+})
+
+
+
+
